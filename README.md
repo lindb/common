2a41bca8110110170c00e730658c095cb0db1ba5
stone1100 <stonejh1100@gmail.com>
2022-07-28 22:05:02 +0800

    [feat]: metric flat protocol/builder

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # common
+
+[![LICENSE](https://img.shields.io/github/license/lindb/common)](https://github.com/lindb/common/blob/main/LICENSE)
+[![Language](https://img.shields.io/badge/Language-Go-blue.svg)](https://golang.org/)
+[![Go Report Card](https://goreportcard.com/badge/github.com/lindb/common)](https://goreportcard.com/report/github.com/lindb/common)
+[![Github Actions Status](https://github.com/lindb/common/workflows/LinDB%20CI/badge.svg)](https://github.com/lindb/common/actions?query=workflow%3A%22LinDB+CI%22)
+[![GoDoc](https://img.shields.io/badge/Godoc-reference-blue.svg)](https://godoc.org/github.com/lindb/common)
+
 Go libraries shared across LinDB and libraries.
---
+++ proto/generate.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# package name in xxx.proto should be xxx
+
+echo "generate pb file"
+
+GO_PREFIX_PATH=github.com/lindb/common/proto
+
+function collect() {
+    # v1/replica.proto
+    file=$1/$(basename $2)
+    base_name=$1/$(basename $2 ".proto")
+    mkdir -p ../gen/$base_name
+    if [[ -z ${GO_OUT_M} ]];then
+        GO_OUT_M="M$file=$GO_PREFIX_PATH/$base_name"
+     else
+        GO_OUT_M="$GO_OUT_M,M$file=$GO_PREFIX_PATH/$base_name"
+     fi
+}
+
+function gen() {
+    dir_name=$1
+    base_name=$(basename $2 ".proto")
+    protoc -I. --gofast_out=plugins=grpc,$GO_OUT_M:../gen/$dir_name/$base_name $2
+}
+
+# brew install flatbuffers
+flatc --go -o ./proto/gen/v1 ./proto/v1/metrics.fbs
+
+# for dir in v1 opentelemetry-v1
+for dir in v1; do
+
+  cd proto/$dir
+
+  for file in `ls *.proto`; do
+    collect $dir $file
+  done
+
+  for file in `ls *.proto`; do
+    gen $dir $file
+  done
+  cd ../../
+  pwd
+done
+
+
