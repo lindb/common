10155891597a621e58b2a69ca200d6f193e3fc66
stone1100 <stonejh1100@gmail.com>
2024-08-06 10:53:25 +0800

    [opt]: add error msg when json marshal fail (#11)

--- pkg/encoding/json.go
+++ pkg/encoding/json.go
@@ -32,12 +32,12 @@ var (
 func JSONMarshal(v interface{}) []byte {
 	data, err := json.Marshal(v)
 	if err != nil {
-		log.Error("json marshal error")
+		log.Error("json marshal error", logger.Error(err))
 	}
 	return data
 }
 
-// JSONUnmarshal parses the JSON-encoded data and stores the result
+// JSONUnmarshal parses the JSON-encoded data and stores the result.
 func JSONUnmarshal(data []byte, v interface{}) error {
 	return json.Unmarshal(data, v)
 }
